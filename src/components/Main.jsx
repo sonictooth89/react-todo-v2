0dd44ee1968090af8e2d656f8b4ecf538c78e7b2
Malgorzata Nowak <nowak.em@interia.eu>
2022-12-03 21:16:00 +0100

    adding items into the list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,40 +1,30 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
    
 function Main () {
-    const [id, setId] = useState(0);
-    const [item, setItem] = useState([]);
+   
+    const [item, setItem] = useState('');
+   
 
-    let items = [
-        {
-            id: 1,
-            text: 'Tidy my room'
-        },
-        {
-            id: 2,
-            text: 'Cooking with Sebastian'
-        }
-    ];
-
-    function handleChange(e){
+    const handleChange = (e) => {
         setItem(e.target.value);
     }
 
-    function handleClick(){
-        setId(prev => ({
-            id: prev.id + 1,
-        }));
-        setItem(item)
-    };
+    const handleClick = () => {
+        const node = document.createTextNode(item);
+        const li = document.createElement('li');
+        li.appendChild(node);
+        const ol = document.getElementById('itemsList');
+        ol.appendChild(li);
+    }
 
     return (
         <>
         <input type="text" value={item} onChange={handleChange} />
         <button onClick={handleClick}>Add</button>
-        <ol>
-            {items.map(item => {
-                return <li key={item.id}>{item.text}</li>
-            })}
+        <ol id="itemsList">
+            
         </ol>
         </>
     )
